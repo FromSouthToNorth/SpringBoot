0be245f8f4d297842ba4592ddf4e53fcf75bfad0
FromSouthToNorth <598050554@qq.com>
2020-10-09 00:40:23 +0800

    ...

---
+++ demo/src/main/java/com/hy/demo/framework/config/ResourcesConfig.java
@@ -0,0 +1,54 @@
+package com.hy.demo.framework.config;
+
+import com.hy.demo.framework.interceptor.RepeatSubmitInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 通用配置
+ */
+@Configuration
+public class ResourcesConfig implements WebMvcConfigurer
+{
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry)
+    {
+
+    }
+
+    /**
+     * 自定义拦截规则
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry)
+    {
+
+    }
+
+    /**
+     * 跨域配置
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry)
+    {
+                // 设置允许跨域的路径
+        registry.addMapping("/**")
+                // 设置允许跨域请求的域名
+                .allowedOrigins("*")
+                // 是否允许证书
+                .allowCredentials(true)
+                // 设置允许的header属性
+                .allowedHeaders("*")
+                // 设置允许的方法
+                .allowedMethods("*");
+    }
+}
