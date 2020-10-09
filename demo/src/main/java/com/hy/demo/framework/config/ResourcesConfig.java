01c5ad6bd6bc6e4b4dc2790a17160de2bdabef77
FromSouthToNorth <598050554@qq.com>
2020-10-09 22:11:29 +0800

    ...

--- demo/src/main/java/com/hy/demo/framework/config/ResourcesConfig.java
+++ demo/src/main/java/com/hy/demo/framework/config/ResourcesConfig.java
@@ -1,6 +1,14 @@
 package com.hy.demo.framework.config;
 
+import com.hy.demo.common.config.DemoConfig;
+import com.hy.demo.common.constant.Constants;
+import com.hy.demo.framework.interceptor.RepeatSubmitInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -12,11 +20,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class ResourcesConfig implements WebMvcConfigurer
 {
+    @Autowired
+    private RepeatSubmitInterceptor repeatSubmitInterceptor;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry)
     {
-
+        /**
+         * 本地文件上传路径
+         */
+        registry.addResourceHandler(Constants.REPEAT_SUBMIT_KEY + "/**").addResourceLocations("file:" + DemoConfig.getProfile() + "/");
     }
 
     /**
@@ -25,24 +38,29 @@ public class ResourcesConfig implements WebMvcConfigurer
     @Override
     public void addInterceptors(InterceptorRegistry registry)
     {
-
+        registry.addInterceptor(repeatSubmitInterceptor).addPathPatterns("/**");
     }
 
     /**
      * 跨域配置
      */
-    @Override
-    public void addCorsMappings(CorsRegistry registry)
+    /**
+     * 跨域配置
+     */
+    @Bean
+    public CorsFilter corsFilter()
     {
-                // 设置允许跨域的路径
-        registry.addMapping("/**")
-                // 设置允许跨域请求的域名
-                .allowedOrigins("*")
-                // 是否允许证书
-                .allowCredentials(true)
-                // 设置允许的header属性
-                .allowedHeaders("*")
-                // 设置允许的方法
-                .allowedMethods("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // 设置访问源地址
+        config.addAllowedOrigin("*");
+        // 设置访问源请求头
+        config.addAllowedHeader("*");
+        // 设置访问源请求方法
+        config.addAllowedMethod("*");
+        // 对接口配置跨域设置
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
---
+++ demo/src/main/java/com/hy/demo/framework/security/handle/AuthenticationEntryPointImpl.java
@@ -0,0 +1,34 @@
+package com.hy.demo.framework.security.handle;
+
+import com.alibaba.fastjson.JSON;
+import com.hy.demo.common.constant.HttpStatus;
+import com.hy.demo.common.core.domain.AjaxResult;
+import com.hy.demo.common.utils.ServletUtils;
+import com.hy.demo.common.utils.StringUtils;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * 认证失败处理类 返回未授权
+ */
+@Component
+public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint, Serializable
+{
+    private static final long serialVersionUID  = -8970718410437077606L;
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse
+            httpServletResponse, AuthenticationException e) throws IOException, ServletException
+    {
+        int code = HttpStatus.UNAUTHORIZED;
+        String msg = StringUtils.format("请求访问：{}，认证失败，无法访问系统资源", httpServletRequest.getRequestURI());
+        ServletUtils.renderString(httpServletResponse, JSON.toJSONString(AjaxResult.error(code, msg)));
+    }
+}
